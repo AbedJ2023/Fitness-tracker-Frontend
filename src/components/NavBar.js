83e402751fcd80275493f444215cebfeb5836269
Abed <abedj2023@Gmail.com>
2023-04-17 01:41:16 -0500

    created a navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+
+const NavBar = ({ token }) => {
+  const handleLogOut = (cb) => {
+    localStorage.removeItem("JWT");
+    cb();
+  };
+
+  return (
+    <header>
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/activities">Activities</Link>
+        <Link to="/Routines">Public Routines</Link>
+        {token ? <Link to="/MyRoutines">My Routines</Link> : null}
+        {token ? (
+          <Link to="/" onClick={handleLogOut}>
+            Log Out
+          </Link>
+        ) : (
+          <>
+            <Link to="/Login">Log in</Link>
+            <Link to="/Register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
