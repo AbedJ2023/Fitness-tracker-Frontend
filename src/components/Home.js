2a4b2f602ddafb403b21a68b0dc9b81ec77aba79
Abed <abedj2023@Gmail.com>
2023-04-18 17:05:15 -0500

    fixed somw bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => {
+const Home = ({ token }) => {
   return (
     <div className="home">
       <h1>Welcome to Fitness Tracker!</h1>
+      {token ? (
+        <>
+          <p> Let's get to work! </p>
+          <div>
+            <Link to="/=">View your routines</Link>
+            <Link to="/">Checkout workout options</Link>
+          </div>
+        </>
+      ) : (
+        <>
+          <p>
+            Not a member? <Link to="/Register">Sign Up</Link> and get to work!
+          </p>
+        </>
+      )}
     </div>
   );
 };
